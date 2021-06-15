b978c3568747f090586168261d174d0cc9d581f3
James <James.Hacklander@gmail.com>
2021-06-14 23:06:05 -0700

    Developed api index, userRoutes. Began developing cart, category, product, and review api routes.

---
+++ controllers/api/cartRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { Cart } = require('../../models');
+// const withAuth = require('../../utils/auth');
+
+// Post Product to cart  (require login session)
+
+// Put update Product Quantity by id  (require login session)
+
+// Delete Product by id  (require login session)
+
+module.exports = router;
---
+++ controllers/api/index.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const cartRoutes = require('./cartRoutes');
+const categoryRoutes = require('./categoryRoutes');
+const productRoutes = require('./productRoutes');
+const reviewRoutes = require('./reviewRoutes');
+
+router.use('/users', userRoutes);
+router.use('/carts', cartRoutes);
+router.use('/categorys', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/reviews', reviewRoutes);
+
+module.exports = router;
+
+
+User, Cart, Category, Product, Review 
