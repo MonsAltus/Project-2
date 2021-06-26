0d0bc491ffac51530b2b3c046174f008b23ea329
James <James.Hacklander@gmail.com>
2021-06-26 09:19:34 -0700

    Modified main.handlebars. Moved logout route from userRoutes to homeRoutes.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -51,17 +51,4 @@ router.post('/login', async (req,res) => {
     }
 });
 
-
-// Logout
-router.post('/logout', (req,res) => {
-    if (req.session.logged_in) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
-    }
-});
-
-
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -133,4 +133,17 @@ router.get('/login', (req,res) => {
     res.render('login');
 });
 
+// Logout
+router.post('/logout', (req,res) => {
+  if (req.session.logged_in) {
+      req.session.destroy(() => {
+          res.status(204).end();
+      });
+      res.redirect('/');
+      return;
+  } else {
+      res.status(404).end();
+  }
+});
+
 module.exports = router;
